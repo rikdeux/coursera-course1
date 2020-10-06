eff9b856a1d54945a4b61413118f68931d41f17d
r.deux <straatjutters@gmail.com>
2020-10-06 20:21:48 +0200

    Starting point

---
+++ module4-javascript/js/script.js
@@ -0,0 +1,32 @@
+// function Dog (name) {
+//     this.name = name;
+// }
+
+// Dog.prototype.bark = function () {
+//     console.log(this.name + " likes barking! Bark!");
+// }
+
+// var max = new Dog("Max", "Buddy");
+// max.bark();
+
+// var array = new Array();
+// array
+
+
+
+
+
+// var counter = 10;
+// var myArray = ["Rik", 2, {handle: "rikdeux"},5];
+// for (var i=0; i < myArray.length; i++) {
+//     counter++;
+// }
+// console.log(counter);
+
+
+
+
+(function () {
+    console.log("Hello");
+})();
+
