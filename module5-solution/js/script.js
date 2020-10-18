4840b3d71c7bc9018961c70669a58acadbcb01b9
r.deux <straatjutters@gmail.com>
2020-10-18 19:49:11 +0200

    wa

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [buildAndShowCategoriesHTML], // ***** <---- TODO: STEP 1: Substitute [...] RIK: ******
+  buildAndShowCategoriesHTML, // ***** <---- TODO: STEP 1: Substitute [...] RIK: ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -104,7 +104,16 @@ function buildAndShowHomeHTML (categories) {
       // var chosenCategoryShortName = ....
       // RIK:
 
-      var chosenCategoryShortName = 
+      var chosenCategoryShortName = chooseRandomCategory (categories);
+      
+      
+      // var chosenCategoryShortName = [""] { 
+      //   chooseRandomCategory(categories)
+      // };
+
+
+      // Hieronder wordt staat de functie chooseRandomCategorie al gedefinieerd. Hierboven moet ik zorgen dat deze wordt uitgevoerd en als chosenCategoryShortname wordt ingesteld.
+    
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -120,13 +129,17 @@ function buildAndShowHomeHTML (categories) {
       // var homeHtmlToInsertIntoMainPage = ....
       // RIK:
 
-      var homeHtmlToInsertIntoMainPage =
+      var homeHtmlToInsertIntoMainPage = buildAndShowHomeHTML (chosenCategoryShortName)
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
       // RIK:
+
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+
+
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
