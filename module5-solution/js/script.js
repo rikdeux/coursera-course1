e6c4ef8eec103444ef2516616e6dbb21d7604311
r.deux <straatjutters@gmail.com>
2020-10-18 21:30:41 +0200

    aight

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -92,7 +92,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-  console.log('adsdasdsa'); 
+  
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
@@ -104,16 +104,11 @@ function buildAndShowHomeHTML (categories) {
       // var chosenCategoryShortName = ....
       // RIK:
 
-      var chosenCategoryShortName = chooseRandomCategory (categories);
+      var randomCategory = chooseRandomCategory (categories);
+      var chosenCategoryShortName = "'" + randomCategory.short_name + "'";
       
-      
-      // var chosenCategoryShortName = [""] { 
-      //   chooseRandomCategory(categories)
-      // };
+      console.log(chosenCategoryShortName);
 
-
-      // Hieronder wordt staat de functie chooseRandomCategorie al gedefinieerd. Hierboven moet ik zorgen dat deze wordt uitgevoerd en als chosenCategoryShortname wordt ingesteld.
-    
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -130,7 +125,9 @@ function buildAndShowHomeHTML (categories) {
       // RIK:
 
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, 'randomCategoryShortName', chosenCategoryShortName);
-      console.log(chosenCategoryShortName)
+      
+      console.log(homeHtmlToInsertIntoMainPage);
+      
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
